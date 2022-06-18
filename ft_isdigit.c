e2b5f7018131742e834d57263e7ab219481dc344
Alp Arslan <hsy.alparslan@gmail.com>
2022-06-18 17:34:44 +0300

    Initial commit

---
+++ ft_isdigit.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+int ft_isdigit(int c)
+	{
+		if (c > 47 && c < 58)
+			return (1);
+		return (0);
+	}
+
---
+++ t_main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int ft_isdigit(int c);
+int main (void)
+	{
+		int c;
+		c = '8';
+		printf("%i", ft_isdigit(c));
+	}
